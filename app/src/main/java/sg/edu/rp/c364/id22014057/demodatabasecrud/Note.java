2645f648ca3110dc302075add223f91315fac33a
xinrou <22014057@myrp.edu.sg>
2023-07-11 22:06:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c364/id22014057/demodatabasecrud/Note.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c364.id22014057.demodatabasecrud;
+
+import android.util.Log;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+
+    private 	int id;
+    private 	String noteContent;
+
+    public Note( int id, String noteContent  ) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {  return id;  }
+
+    public String getNoteContent() { return noteContent; }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    @Override
+    public String toString() { return "ID:" + id + ", " + noteContent;  }
+
+}
+
